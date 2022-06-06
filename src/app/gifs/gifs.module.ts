453fe981a16affcc3fd026e728d31143a94ce990
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-06 07:43:50 -0600

    Components for search and results are ready.

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifPageComponent } from './components/gif-page/gif-page.component';
+import { SearchComponent } from './components/search/search.component';
+import { ResultsComponent } from './components/results/results.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GifPageComponent,
+    SearchComponent,
+    ResultsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    GifPageComponent
   ]
 })
 export class GifsModule { }
