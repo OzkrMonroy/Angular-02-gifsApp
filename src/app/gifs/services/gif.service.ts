cd979fdbbb75f4e067b2ad152dd5c183122c5dad
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-06 20:19:17 -0600

    Results and history are saved in the local storage.

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -10,7 +10,10 @@ export class GifService {
   private _history: string[] = [];
   public gifsResult: Gif[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this._history = JSON.parse(localStorage.getItem('history') || '[]');
+    this.gifsResult = JSON.parse(localStorage.getItem('lastResults') || '[]');
+  }
 
   get history(): string[] {
     return [...this._history]
@@ -20,10 +23,13 @@ export class GifService {
     if(!this._history.includes(word)){
       this._history.unshift(word)
       this._history = this._history.splice(0, 10);
+      localStorage.setItem('history', JSON.stringify(this._history))
+
       this.http.get<GIFs>(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${word}&limit=10`)
         .subscribe({
           next: (result) => {
             this.gifsResult = result.data
+            localStorage.setItem('lastResults', JSON.stringify(result.data))
           }
         })
     }
